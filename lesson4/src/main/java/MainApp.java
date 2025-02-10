800b025e16668c0bd3dd2cacd1c1a6ed4363e11f
s.makarov <smakarov@avo.uz>
2025-02-10 20:39:57 +0300

    lesson 4 - 1

--- lesson4/src/main/java/MainApp.java
+++ lesson4/src/main/java/MainApp.java
@@ -6,23 +6,21 @@ public class MainApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the first number: ");
+        System.out.println("PLEASE ENTER THE FIRST NUMBER: ");
         int number1 = scanner.nextInt();
-        System.out.println("Please enter the second number: ");
+        System.out.println("PLEASE ENTER THE SECOND NUMBER: ");
         int number2 = scanner.nextInt();
 
         int sum = 0;
         int count = 0;
 
-        for (int i = number1; i <= number2; i++) {
-
-            sum = sum + i;
+        while (number1 <= number2) {
+            sum += number1;
             count++;
-
+            number1++;
         }
 
         System.out.println(sum/count);
-
 
     }
 
